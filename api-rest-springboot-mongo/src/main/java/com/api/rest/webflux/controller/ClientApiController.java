285a21b20b2e1fc51350a8e2fa33d15ae886d770
Alelizzt <alejandrolistc@hotmail.com>
2024-10-07 20:50:58 -0500

    add save client method on controller

--- api-rest-springboot-mongo/src/main/java/com/api/rest/webflux/controller/ClientApiController.java
+++ api-rest-springboot-mongo/src/main/java/com/api/rest/webflux/controller/ClientApiController.java
@@ -2,23 +2,30 @@ package com.api.rest.webflux.controller;
 
 import java.io.File;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 import com.api.rest.webflux.documents.ClientApi;
 import com.api.rest.webflux.services.ClientApiService;
 
+import jakarta.validation.Valid;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -77,4 +84,32 @@ public class ClientApiController {
 			.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 	
+	@PostMapping
+	public Mono<ResponseEntity<Map<String, Object>>> saveClient(@Valid @RequestBody Mono<ClientApi> monoClient) {
+		Map<String, Object> response = new HashMap<>();
+		
+		return monoClient.flatMap(client -> {
+			return clientApiService.save(client).map(c -> {
+				response.put("client", c);
+				response.put("message", "Client saved succesfully");
+				response.put("timestamp", new Date());
+				return ResponseEntity.created(URI.create("/api/clients/".concat(c.getId())))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(response);
+			});
+		}).onErrorResume(t -> {
+			return Mono.just(t).cast(WebExchangeBindException.class)
+					.flatMap(e -> Mono.just(e.getFieldErrors()))
+					.flatMapMany(Flux::fromIterable)
+					.map(fieldError -> "Field: " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+					.collectList()
+					.flatMap(list -> {
+						response.put("errors", list);
+						response.put("timestamp", new Date());
+						response.put("status", HttpStatus.BAD_REQUEST.value());
+						
+						return Mono.just(ResponseEntity.badRequest().body(response));
+					});
+		});
+	}
 }
