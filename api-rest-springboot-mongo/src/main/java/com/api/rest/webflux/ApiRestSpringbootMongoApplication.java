00497cd65cccea801cefd750b47ca0870ebcf1e3
Alelizzt <alejandrolistc@hotmail.com>
2024-10-07 19:00:45 -0500

    Project configuration

---
+++ api-rest-springboot-mongo/src/main/java/com/api/rest/webflux/ApiRestSpringbootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootMongoApplication.class, args);
+	}
+
+}
